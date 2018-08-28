5f21cdcd7d3001d8f0bb302a879cfd4550f825ea
Liam Bigelow <liambigelow@cloudcannon.com>
2018-08-28 16:45:59 +1200

    Updates

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const sslChecker =  require("ssl-checker");
-
-var args = process.argv.slice(2);
-
-args.forEach(function (val, index, array) {
-    let lval = val;
+const args = process.argv.slice(2);
+args.forEach(function (val) {
+    const lval = val;
     sslChecker(lval).then((log) => {
-        console.log(lval);
-        console.log(log);
+        console.log({domain: lval, ...log});
     }).catch(console.error);
 });
